3f6e6542c609db570dd94b0d54e71c9ebb57f2f1
shahzad6077 <alim6077@gmail.com>
2021-01-14 16:56:41 +0500

    Import Taskbox Components from  Bit/dev and use in App.

--- README.md
+++ README.md
@@ -7,14 +7,14 @@
 - Storybook (Designing Sys & Documentation)
 - Surge (Hosting)
 
-## `Check the below link to visit the app`
+###### `Check the below link to visit the app`
 
 Open [taskbox-shahzad.surge.sh](https://taskbox-shahzad.surge.sh/) to view it in the browser.
 
-## `Check the below link to visit the Component Collection of Taskbox.`
+###### `Visit the Storybook.`
 
-Open [https://bit.dev/shahzad6077/taskbox](https://bit.dev/shahzad6077/taskbox) to view it in the browser.
+Open [taskbox-shahzad.surge.sh](https://story-taskbox-shahzad.surge.sh/) to view it in the browser.
 
-## `Check the below link to visit the Component Collection of Taskbox.`
+###### `Check the below link to visit the Component Collection of Taskbox.`
 
 Open [https://bit.dev/shahzad6077/taskbox](https://bit.dev/shahzad6077/taskbox) to view it in the browser.
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React, { useState } from "react";
-import Taskbox, { StatusInterface } from "./Components/Taskbox";
+import TaskList, { TaskListProps } from "@bit/shahzad6077.taskbox.task-list";
+import { TaskboxProps } from "@bit/shahzad6077.taskbox.task-list/dist/Taskbox";
 
+const LIST: TaskboxProps[] = [
+  { id: "123", label: "Pushups game on 23Aug", status: "DEFAULT" },
+  { id: "456", label: "Leg game on 24Aug", status: "PINNED" },
+  { id: "578", label: "Biceps game on 25Aug", status: "DEFAULT" },
+  { id: "235", label: "Chest game on 26Aug", status: "COMPLETED" },
+];
 function App() {
+  const [list, setList] = useState<TaskboxProps[]>(LIST);
+  const onChangeTaskItem = (id: string, status: any) => {
+    const shallow = [...list];
+    setList(
+      shallow.map((obj) => {
+        if (obj.id === id) {
+          return { ...obj, status: status };
+        } else {
+          return obj;
+        }
+      })
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h2>Taskbox app</h2>
       </header>
+      <TaskList
+        title="GYM TASKS"
+        style={{ width: "420px" }}
+        list={list}
+        onChangeTaskItem={onChangeTaskItem}
+      />
     </div>
   );
 }
